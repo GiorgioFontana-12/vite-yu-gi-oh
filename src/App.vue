deaf6cca10a0d44a119e80859b9d88d34054ec4a
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-05-23 19:12:13 +0200

    error error error

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import { storeFigurine } from '../src/components/store';
+
+export default {
+  name: "App",
+  components: {
+  },
+  data() {
+    return {
+      storeFigurine
+    }
+  },
+  mounted() {
+    console.log(this.storeFigurine.urlAPIFigurine)
+
+    axios.get(this.storeFigurine.urlAPIFigurine).then(r => {
+      //Se completata correttamente stampo il dato
+      console.log("Ricevuto: ", r);
+      //Copio i dati nello store per riutilizzarli in altri componenti
+      this.store.personaggi = r;
+    })
+  }
+}
+</script>
+
+<template>
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/store.js
@@ -0,0 +1,7 @@
+import {reactive} from 'vue'
+
+export const storeFigurine = reactive({
+    loading: true,
+    urlAPIFigurine: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+
+});
