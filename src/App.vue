29a389542a0d79b7e123c140e18805e595c6ea9e
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-05-24 01:51:32 +0200

    import image in html

--- src/App.vue
+++ src/App.vue
@@ -20,13 +20,37 @@ export default {
       //Copio i dati nello store per riutilizzarli in altri componenti
       this.storeFigurine.figurine = r.data.data
       console.log(this.storeFigurine.figurine)
+      console.log(this.storeFigurine.figurine[0].card_images[0].image_url)
+
     })
   }
 }
 </script>
 
 <template>
+  <header>
+     <h1>Yu-Gi-Oh-Api</h1>
+    
+  </header>
+  <main>
+    <div v-for="(img, i) in storeFigurine.figurine"> 
+      <img :src=" img.card_images[0].image_url " alt="">
+    </div>
+  </main>
+
 </template>
 
 <style lang="scss" scoped>
+*{
+  margin: 0 ;
+  padding: 0 ;
+  box-sizing: border-box ;
+}
+  header{
+    height: 80px;
+  }
+  main{
+    background-color: orange;
+    padding: 80px ;
+  }
 </style>
